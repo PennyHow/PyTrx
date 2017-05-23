cb5f338222bcf1883cef7324bdac9c56d962e7f4
Penny How <p.how@ed.ac.uk>
2017-05-23 14:43:18 +0200

    Supraglacial lake area example added

---
+++ Examples/driver_autoarea.py
@@ -0,0 +1,154 @@
+'''
+PYTRX EXAMPLE AUTOMATED AREA DRIVER
+
+This script is part of PyTrx, an object-oriented programme created for the 
+purpose of calculating real-world measurements from oblique images and 
+time-lapse image series.
+
+This driver calculates supraglacial lake surface area at Kronebreen, Svalbard, 
+for a small subset of the 2014 melt season using modules in PyTrx. Specifically 
+this script performs automated detection of supraglacial lakes through 
+sequential images of the glacier to derive surface areas which have been 
+corrected for image distortion.
+
+@author: Penny How (p.how@ed.ac.uk)
+         Nick Hulton (nick.hulton@ed.ac.uk)
+
+'''
+
+#Import packages
+import sys
+import time
+import math
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from scipy import interpolate
+
+#Import PyTrx packages
+sys.path.append('../')
+from Measure import Area
+from CamEnv import CamEnv
+from Images import TimeLapse
+from FileHandler import writeHomographyFile, writeVelocityFile
+from Utilities import plotVelocity, interpolateHelper, plotInterpolate
+
+
+#-----------------------------   Map data files   -----------------------------
+
+#Define data inputs
+camdata = '../Examples/camenv_data/camenvs/CameraEnvironmentData_cam2_2014.txt'
+cammask = '../Examples/camenv_data/masks/c5_2014_amask.JPG'
+camimgs = '../Examples/images/KR5_2014_subset/*.JPG'
+
+#Define data output directory
+destination = '..Results/KR5_2014_lakes/'
+
+
+#-------------------------   Create camera object   ---------------------------
+#Define camera environment
+cameraenvironment = CamEnv(camdata)
+
+
+##Get PP and approximate YPR
+#ypr_data = fileDirectory + 'Data/GCPdata/gcps/c5_2014_ypr.txt'
+#camera = cam5.getFullCamera()
+#print 'Full camera info: ' + str(camera)
+#cam5.showPrincipalPoint()
+#cam5.approxYPR(ypr_data)
+#print 'Camera pose: ' 
+#print cam5.getCameraPose()
+
+
+##Show viewshed
+#demobj = DEM_FromMat('C:/Users/s0824923/Local Documents/python_workspace/pytrx/Data/GCPdata/dem/dem.mat')
+#
+#X = demobj.getData(0)
+#Y = demobj.getData(1)
+#Z = demobj.getZ()
+#cellsize = X[0][1]-X[0][0]
+#
+#ext = demobj.getExtent()
+#
+#camxyz = [451632.669, 8754648.786, 624.699]
+#
+#vis = voxelviewshed(X,Y,Z,camxyz)
+#vis = np.flipud(vis)
+#print vis.shape
+#
+#plt.imshow(vis, cmap='gray', origin ='upper', extent=ext)
+#plt.scatter(camxyz[0], camxyz[1], color = 'r')
+#plt.show()
+
+
+#------------------------   Calculate lake areas   ----------------------------
+
+#Set Area class initialisation variables
+calibFlag=True
+maxim = 0 
+imband = 'L'
+loadall = False
+timem = 'EXIF'
+
+#Set up Area object, from which areal extent will be measured
+lakes = Area(camimgs, cameraenvironment, calibFlag, cammask, maxim, imband,
+             loadall, time,)
+                 
+#Set image enhancement parameters. If these are undefined then they will be 
+#set to a default enhancement of ('light', 50, 20)
+lakes.setEnhance('light', 50, 20)
+
+##Show example of image enhancement
+#im=lakes.getMaxImgData()
+#im=lakes.maskImg(im)  
+#im=lakes.enhanceImg(im)
+#plt.imshow(im)
+#plt.show()
+
+#Set colour range, from which extents will be distinguished. If colour range 
+#is not specified, it will be manually defined 
+lakes.setColourrange(8, 1)                                                                                                                                                      
+
+#Set plotting extent for easier colour range definition and area verification
+lakes.setPXExt(0,1200,2000,1500)
+
+##Set polygon threshold (i.e. number of polygons kept)
+#lakes.setThreshold(20)
+
+##Calculate pixel extents. Use this function if pixel extents are only needed, 
+##or a DEM is not available. Pixel extents are automatically calculated if 
+##calcAreas is called.
+#polys, areas = lakes.calcExtents()
+
+#Calculate real areas
+rpolys, rareas = lakes.calcAreas(method='auto', color=False, verify=False)
+
+##Import data from file
+#rpolys, rareas, pxpolys, pxareas = lakes.importData(destination)
+
+
+#----------------------------   Export data   ---------------------------------
+##Write results to file
+#lakes.writeData(destination)
+#
+##Create shapefiles
+#target = destination + 'shpfiles/'
+#proj = 32633
+#lakes.exportSHP(target, proj) 
+
+
+#Write all image extents and dems to destination file
+target=destination + 'extentimgs/'
+length=len(rpolys)
+for i in range(length):
+    lakes.plotPX(i, target)
+    lakes.plotXYZ(i, target, dem=False, show=False)
+
+
+#Show image path list for checking
+lakes.printImageList() 
+
+
+#------------------------------------------------------------------------------                                                                                                                                                                                                                                                                                                      
+print 'Finished'
+
---
+++ Examples/driver_velocity.py
@@ -0,0 +1,141 @@
+'''
+PYTRX EXAMPLE VELOCITY DRIVER
+
+This script is part of PyTrx, an object-oriented programme created for the 
+purpose of calculating real-world measurements from oblique images and 
+time-lapse image series.
+
+This driver calculates surface velocities at Kronebreen, Svalbard, for the 2014 
+melt season using modules in PyTrx. Specifically this script performs feature
+tracking through sequential daily images of the glacier to derive surface
+velocities (spatial average, individual point displacements and interpolated
+velocity maps) which have been corrected for image distortion and camera 
+homography.
+
+@author: Penny How (p.how@ed.ac.uk)
+         Nick Hulton (nick.hulton@ed.ac.uk)
+
+'''
+
+#Import packages
+import sys
+import time
+import math
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from scipy import interpolate
+
+#Import PyTrx packages
+sys.path.append('../')
+from CamEnv import CamEnv
+from Images import TimeLapse
+from FileHandler import writeHomographyFile, writeVelocityFile
+from Utilities import plotVelocity, interpolateHelper, plotInterpolate
+
+
+#-------------------------   Map data sources   -------------------------------
+
+#Get data needed for processing
+camdata = '../Examples/camenv_data/camenvs/CameraEnvironmentData_cam2_2014.txt'
+camvmask = '../Examples/camenv_data/masks/c2_2014_vmask.JPG'
+caminvmask = '../Examples/camenv_data/invmasks/c2_2014_inv.JPG'
+camimgs = '../Examples/images/KR2_2014_subset/*.JPG'
+
+
+#Define data output directory
+destination = '../Examples/results/KR2_2014_velocities/'
+
+
+#-----------------------   Create camera object   -----------------------------
+
+#Define camera environment
+cameraenvironment = CamEnv(camdata, quiet=2)
+#cameraenvironment.report()
+
+
+#----------------------   Calculate velocities   ------------------------------
+
+#Set up TimeLapse object
+tl=TimeLapse(camimgs, cameraenvironment, camvmask, caminvmask, image0=0, 
+             band='L', quiet=2) 
+
+#Calculate homography and velocities    
+hg, outputV = tl.calcVelocities()
+   
+   
+#----------------------------   Plot Results   --------------------------------
+
+print '\nData plotting...'
+plotcams = False
+plotcombined = False
+plotspeed = False
+plotmaps = False
+save = False
+
+
+#Get DEM from camera environment object
+dem=cameraenvironment.getDEM()
+
+#Set extent
+xmin=446000
+xmax=451000
+ymin=8754000
+ymax=8760000
+
+demex=dem.getExtent()
+xscale=dem.getCols()/(demex[1]-demex[0])
+yscale=dem.getRows()/(demex[3]-demex[2])
+
+xdmin=(xmin-demex[0])*xscale
+xdmax=((xmax-demex[0])*xscale)+1
+ydmin=(ymin-demex[2])*yscale
+ydmax=((ymax-demex[2])*yscale)+1
+    
+demred=dem.subset(xdmin,xdmax,ydmin,ydmax)
+lims=demred.getExtent()
+demred=demred.getZ()
+
+
+span=[0,-1]
+im1=tl.getImageObj(0)
+
+for i in range(tl.getLength()-1)[span[0]:span[1]]:
+    for vel in outputV:
+        im0=im1
+        im1=tl.getImageObj(i+1)
+        plotVelocity(vel,im0,im1,cameraenvironment,demred,lims,None,
+                     plotcams,plotcombined,plotspeed,plotmaps)
+
+for vel in outputV:
+    xy1 = vel[0][0]
+    xy2 = vel[0][1]
+    method='linear'
+
+    grid, pointsextent = interpolateHelper(xy1,xy2,method,filt=False)
+    fgrid, fpointsextent = interpolateHelper(xy1,xy2,method,filt=True)
+    
+    print 'Plotting unfiltered velocity map...'
+    plotInterpolate(demred, lims, grid, pointsextent, 
+                    save=destination+'interp1.jpg')
+                    
+    print 'Plotting filtered velocity map...'
+    plotInterpolate(demred, lims, fgrid, fpointsextent, 
+                    save=destination+'interp2.jpg')    
+
+
+#---------------------------  Export data   -----------------------------------
+
+print '\nBeginning file exporting...'
+
+#Write homography data to .csv file
+target1 = destination + 'homography.csv'
+writeHomographyFile(hg,tl,target1)
+
+#Write out velocity data to .csv file
+target2 = destination + 'velo_output.csv'
+writeVelocityFile(outputV, tl, target2) 
+
+
+#------------------------------------------------------------------------------
+print 'Finished'
