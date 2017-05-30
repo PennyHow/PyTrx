1a49a0ce407730c634ddc6a988f0b0794fe4458b
Penny How <p.how@ed.ac.uk>
2017-05-30 14:12:35 +0200

    Automated area detection example updated

--- Examples/driver_autoarea.py
+++ Examples/driver_autoarea.py
@@ -19,19 +19,13 @@ corrected for image distortion.
 #Import packages
 import sys
 import time
-import math
-import numpy as np
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from scipy import interpolate
 
 #Import PyTrx packages
 sys.path.append('../')
 from Measure import Area
 from CamEnv import CamEnv
-from Images import TimeLapse
 from FileHandler import writeAreaFile, writeAreaSHP
-from Utilities import plotAreaPX, plotAreaXYZ
+from Utilities import plotPX, plotXYZ
 
 
 #-----------------------------   Map data files   -----------------------------
@@ -42,46 +36,14 @@ cammask = '../Examples/camenv_data/masks/c5_2014_amask.JPG'
 camimgs = '../Examples/images/KR5_2014_subset/*.JPG'
 
 #Define data output directory
-destination = '../results/KR5_autoarea/'
+destination = '../Examples/results/KR5_autoarea/'
 
 
-#-------------------------   Create camera object   ---------------------------
+#--------------------   Create camera and area objects   ----------------------
+
 #Define camera environment
 cameraenvironment = CamEnv(camdata)
 
-
-##Get PP and approximate YPR
-#ypr_data = fileDirectory + 'Data/GCPdata/gcps/c5_2014_ypr.txt'
-#camera = cam5.getFullCamera()
-#print 'Full camera info: ' + str(camera)
-#cam5.showPrincipalPoint()
-#cam5.approxYPR(ypr_data)
-#print 'Camera pose: ' 
-#print cam5.getCameraPose()
-
-
-##Show viewshed
-#demobj = DEM_FromMat('C:/Users/s0824923/Local Documents/python_workspace/pytrx/Data/GCPdata/dem/dem.mat')
-#
-#X = demobj.getData(0)
-#Y = demobj.getData(1)
-#Z = demobj.getZ()
-#cellsize = X[0][1]-X[0][0]
-#
-#ext = demobj.getExtent()
-#
-#camxyz = [451632.669, 8754648.786, 624.699]
-#
-#vis = voxelviewshed(X,Y,Z,camxyz)
-#vis = np.flipud(vis)
-#print vis.shape
-#
-#plt.imshow(vis, cmap='gray', origin ='upper', extent=ext)
-#plt.scatter(camxyz[0], camxyz[1], color = 'r')
-#plt.show()
-
-
-#------------------------   Calculate lake areas   ----------------------------
 
 #Define Area class initialisation variables
 method = 'auto'             #Method for detection ('auto' or 'manual')
@@ -95,62 +57,61 @@ quiet = 2                   #Level of commentary
 #Set up Area object, from which areal extent will be measured
 lakes = Area(camimgs, cameraenvironment, method, calibFlag, cammask, maxim, 
              imband, quiet, loadall, time)
-                
+
+
+#---------------------   Set area detection parameters   ----------------------             
+
 #Set image enhancement parameters. If these are undefined then they will be 
 #set to a default enhancement of ('light', 50, 20)
 lakes.setEnhance('light', 50, 20)
 
-##Show example of image enhancement
-#im=lakes.getMaxImgData()
-#im=lakes.maskImg(im)  
-#im=lakes.enhanceImg(im)
-#plt.imshow(im)
-#plt.show()
 
 #Set colour range, from which extents will be distinguished. If colour range 
 #is not specified, it will be manually defined 
 lakes.setColourrange(8, 1)                                                                                                                                                      
 
+
 #Set plotting extent for easier colour range definition and area verification
 lakes.setPXExt(0,1200,2000,1500)
 
+
 #Set polygon threshold (i.e. number of polygons kept)
 lakes.setThreshold(5)
+
+
+#-------------------------   Calculate areas   --------------------------------
+
+#Calculate real areas
+rpolys, rareas = lakes.calcAreas(color=False, verify=False)
 
 ##Calculate pixel extents. Use this function if pixel extents are only needed, 
 ##or a DEM is not available. Pixel extents are automatically calculated if 
 ##calcAreas is called.
 #polys, areas = lakes.calcExtents()
 
-#Calculate real areas
-rpolys, rareas = lakes.calcAreas(color=False, verify=False)
-
 ##Import data from file
 #rpolys, rareas, pxpolys, pxareas = lakes.importData(destination)
 
 
 #----------------------------   Export data   ---------------------------------
+
 #Write results to file
 writeAreaFile(lakes, destination)
 
+
 #Create shapefiles
-target = destination + 'shpfiles/'
+target1 = destination + 'shpfiles/'
 proj = 32633
-writeAreaSHP(target, proj) 
+writeAreaSHP(lakes, target1, proj) 
 
 
-#Write all image extents and dems to destination file
-target=destination + 'extentimgs/'
-length=len(rpolys)
-for i in range(length):
-    plotAreaPX(i, None)
-    plotAreaXYZ(i, None, dem=True, show=True)
-
-
-#Show image path list for checking
-lakes.printImageList() 
+#Write all image extents and dems 
+target2 = destination + 'outputimgs/'
+for i in range(len(rpolys)):
+    plotPX(lakes, i, destination, crop=False)
+    plotXYZ(lakes, i, destination, dem=False, show=True)
 
 
 #------------------------------------------------------------------------------                                                                                                                                                                                                                                                                                                      
-print 'Finished'
 
+print 'Finished'
