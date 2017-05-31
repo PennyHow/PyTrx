daaf75adf68723d472afe0399a63bc2dc6a73b27
Penny How <p.how@ed.ac.uk>
2017-05-31 22:51:24 +0200

    Manual plume detection driver and data updated

--- Examples/driver_manualarea.py
+++ Examples/driver_manualarea.py
@@ -31,12 +31,11 @@ from Utilities import plotPX, plotXYZ
 #-----------------------------   Map data files   -----------------------------
 
 #Define data inputs
-camdata = '../Examples/camenv_data/camenvs/CameraEnvironmentData_cam5_2014.txt'
-cammask = '../Examples/camenv_data/masks/c5_2014_amask.JPG'
-camimgs = '../Examples/images/KR5_2014_subset/*.JPG'
+camdata = '../Examples/camenv_data/camenvs/CameraEnvironmentData_cam1_2014.txt'
+camimgs = '../Examples/images/KR1_2014_subset/*.JPG'
 
 #Define data output directory
-destination = '../Examples/results/KR5_autoarea/'
+destination = '../Examples/results/KR1_manualarea/'
 
 
 #--------------------   Create camera and area objects   ----------------------
@@ -46,7 +45,7 @@ cameraenvironment = CamEnv(camdata)
 
 
 #Define Area class initialisation variables
-method = 'auto'             #Method for detection ('auto' or 'manual')
+method = 'manual'           #Method for detection ('auto' or 'manual')
 calibFlag = True            #Detect with corrected or uncorrected images
 maxim = 0                   #Image number of maximum areal extent 
 imband = 'L'                #Desired image band
@@ -55,34 +54,14 @@ timem = 'EXIF'              #Method to derive image times
 quiet = 2                   #Level of commentary
 
 #Set up Area object, from which areal extent will be measured
-lakes = Area(camimgs, cameraenvironment, method, calibFlag, cammask, maxim, 
+plumes = Area(camimgs, cameraenvironment, method, calibFlag, None, maxim, 
              imband, quiet, loadall, time)
-
-
-#---------------------   Set area detection parameters   ----------------------             
-
-#Set image enhancement parameters. If these are undefined then they will be 
-#set to a default enhancement of ('light', 50, 20)
-lakes.setEnhance('light', 50, 20)
-
-
-#Set colour range, from which extents will be distinguished. If colour range 
-#is not specified, it will be manually defined 
-lakes.setColourrange(8, 1)                                                                                                                                                      
-
-
-#Set plotting extent for easier colour range definition and area verification
-lakes.setPXExt(0,1200,2000,1500)
-
-
-#Set polygon threshold (i.e. number of polygons kept)
-lakes.setThreshold(5)
 
 
 #-------------------------   Calculate areas   --------------------------------
 
 #Calculate real areas
-rpolys, rareas = lakes.calcAreas(color=False, verify=False)
+rpolys, rareas = plumes.calcAreas(color=False, verify=False)
 
 ##Calculate pixel extents. Use this function if pixel extents are only needed, 
 ##or a DEM is not available. Pixel extents are automatically calculated if 
@@ -96,20 +75,20 @@ rpolys, rareas = lakes.calcAreas(color=False, verify=False)
 #----------------------------   Export data   ---------------------------------
 
 #Write results to file
-writeAreaFile(lakes, destination)
+writeAreaFile(plumes, destination)
 
 
 #Create shapefiles
 target1 = destination + 'shpfiles/'
 proj = 32633
-writeAreaSHP(lakes, target1, proj) 
+writeAreaSHP(plumes, target1, proj) 
 
 
 #Write all image extents and dems 
 target2 = destination + 'outputimgs/'
 for i in range(len(rpolys)):
-    plotPX(lakes, i, destination, crop=False)
-    plotXYZ(lakes, i, destination, dem=False, show=True)
+    plotPX(plumes, i, destination, crop=False)
+    plotXYZ(plumes, i, destination, dem=False, show=True)
 
 
 #------------------------------------------------------------------------------                                                                                                                                                                                                                                                                                                      
