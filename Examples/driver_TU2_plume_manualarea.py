d06b323ee602440f474b3aaa278783a828297fed
Penny How <p.how@ed.ac.uk>
2017-08-07 21:13:27 +0200

    TU4 camera information added

---
+++ Examples/driver_TU2_plume_manualarea.py
@@ -0,0 +1,122 @@
+'''
+PYTRX EXAMPLE MANUAL AREA DRIVER
+
+This script is part of PyTrx, an object-oriented programme created for the 
+purpose of calculating real-world measurements from oblique images and 
+time-lapse image series.
+
+This driver calculates meltwater plume surface extent at Tunabreen, Svalbard, 
+for a small subset of the 2015 melt season using modules in PyTrx. Specifically 
+this script performs manual detection of meltwater plumes through 
+sequential images of the glacier to derive surface areas which have been 
+corrected for image distortion.
+
+@author: Penny How (p.how@ed.ac.uk)
+         Nick Hulton (nick.hulton@ed.ac.uk)
+
+'''
+
+#Import packages
+import sys
+import time
+import os
+
+#Import PyTrx packages
+sys.path.append('../')
+from Measure import Area
+from CamEnv import CamEnv
+from FileHandler import writeAreaFile, writeSHPFile, importMeasureData
+from Utilities import plotPX, plotXYZ
+
+
+#-----------------------------   Map data files   -----------------------------
+
+#Define plume detection specifications
+camera = 'TU2'
+date = '150907'
+plume = 'plume1'
+
+
+#Define data inputs
+camdata = '../Examples/camenv_data/camenvs/CameraEnvironmentData_TU2_2015.txt'
+camimgs = ('F:/imagery/tunabreen/pytrx/' + camera + 
+           '_hourly_2015/' + date + '/*.JPG')
+
+
+#Define data output directory
+destination = ('../Examples/results/tuna_plume/' + camera + '/' + 
+               date + '/' + plume + '/')
+
+
+#--------------------   Create camera and area objects   ----------------------
+
+#Define camera environment
+cameraenvironment = CamEnv(camdata)
+
+#Show GCPs
+extent=[550000,565000,8706000,8716000]
+cameraenvironment.showGCPs(extent)
+
+##Show Principal Point
+#cameraenvironment.showPrincipalPoint()
+
+
+#Define Area class initialisation variables
+calibFlag = True            #Detect with corrected or uncorrected images
+maxim = 0                   #Image number of maximum areal extent 
+imband = 'L'                #Desired image band
+loadall = False             #Load all images?
+timem = 'EXIF'              #Method to derive image times
+quiet = 2                   #Level of commentary
+
+#Set up Area object, from which areal extent will be measured
+plumes = Area(camimgs, cameraenvironment, calibFlag, None, maxim, imband, 
+              quiet, loadall, time)
+
+
+#-------------------------   Calculate areas   --------------------------------
+
+#Calculate real areas
+rpolys, rareas = plumes.calcManualAreas()
+
+#plumes.setColourrange(8, 1)
+#plumes.setEnhance('light', 50, 20)
+#pxpolys, pxareas = plumes.calcAutoExtents(True, True)
+#rpolys, rareas = plumes.calcAutoAreas()
+
+
+##Calculate pixel extents. Use this function if pixel extents are only needed, 
+##or a DEM is not available. Pixel extents are automatically calculated if 
+##calcAreas is called.
+#polys, areas = plumes.calcExtents()
+
+##Import data from file
+#rpolys, rareas, pxpolys, pxareas = importMeasureData(plumes, destination)
+
+
+#----------------------------   Export data   ---------------------------------
+
+#Write results to file
+writeAreaFile(plumes, destination)
+
+
+#Create shapefiles
+target1 = destination + 'shpfiles/'
+if not os.path.exists(target1):
+    os.makedirs(target1)
+proj = 32633
+writeSHPFile(plumes, target1, proj) 
+
+
+#Write all image extents and dems 
+target2 = destination + 'outputimgs/'
+if not os.path.exists(target2):
+    os.makedirs(target2)
+for i in range(len(rpolys)):
+    plotPX(plumes, i, target2, show=False, crop=False)
+    plotXYZ(plumes, i, target2, dem=True, show=False)
+
+
+#------------------------------------------------------------------------------                                                                                                                                                                                                                                                                                                      
+
+print '\n\nFINISHED'
---
+++ Examples/results/tuna_plume/TU4/150819/README.txt
@@ -0,0 +1,25 @@
+Images out of sequence:
+IMG_1646.JPG: you clicked 82 points ***
+IMG_1592.JPG: you clicked 162 points
+IMG_1598.JPG: you clicked 197 points
+IMG_1604.JPG: you clicked 129 points
+IMG_1610.JPG: you clicked 149 points
+IMG_1616.JPG: you clicked 178 points
+IMG_1622.JPG: you clicked 0 points
+IMG_1628.JPG: you clicked 0 points
+IMG_1634.JPG: you clicked 0 points
+IMG_1640.JPG: you clicked 158 points ***
+IMG_1652.JPG: you clicked 151 points ***
+IMG_1658.JPG: you clicked 92 points
+IMG_1664.JPG: you clicked 87 points
+IMG_1670.JPG: you clicked 69 points
+IMG_1676.JPG: you clicked 100 points
+IMG_1682.JPG: you clicked 107 points
+IMG_1688.JPG: you clicked 128 points
+IMG_1694.JPG: you clicked 171 points
+IMG_1700.JPG: you clicked 194 points
+IMG_1706.JPG: you clicked 104 points
+IMG_1730.JPG: you clicked 96 points
+IMG_1712.JPG: you clicked 142 points
+IMG_1718.JPG: you clicked 147 points
+IMG_1724.JPG: you clicked 158 points
