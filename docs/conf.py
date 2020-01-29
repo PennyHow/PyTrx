22a9d792189c87f1dc59bed38421b5446041782f
Penny How <pennyruthhow@gmail.com>
2020-01-28 21:36:31 -0300

    readthedocs test

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.PyTrx'))
+sys.path.insert(0, os.path.abspath('../../PyTrx'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
