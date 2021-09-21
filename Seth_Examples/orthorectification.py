c552b00555858b1086525ba9282a6d88bb36d81d
Penny How <pennyruthhow@gmail.com>
2021-09-21 16:20:49 +0200

    georectification issue solved (HOW)

--- Seth_Examples/orthorectification.py
+++ Seth_Examples/orthorectification.py
@@ -12,9 +12,10 @@ from scipy.interpolate import griddata
 sys.path.append('../')
 import osgeo.ogr as ogr
 import osgeo.osr as osr
+from scipy import interpolate
 
-from pyproj import Proj
-from CamEnv import GCPs, CamEnv, setProjection, projectUV, projectXYZ, optimiseCamera
+#from pyproj import Proj
+from CamEnv import GCPs, CamEnv, setProjection, projectUV, projectXYZ, optimiseCamera, computeResidualsXYZ
 import DEM
 
 # =============================================================================
@@ -37,7 +38,7 @@ ingleCam = CamEnv(ingleCamEnv)
 # Get DEM from camera environment
 dem = ingleCam.getDEM()
 
-# Define GCP class
+# Get GCPs
 inglefield_gcps = directory + '/cam_env/GCPs_20190712.txt'
 gcps = GCPs(dem, inglefield_gcps, ingle_calibimg)
 ingle_xy = gcps.getGCPs()
@@ -45,46 +46,34 @@ ingle_xy = gcps.getGCPs()
 # Report calibration data
 ingleCam.reportCalibData()
 
-#Show GCPs                           
-ingleCam.showGCPs()
 
-#Show Camera plots                          
-ingleCam.showPrincipalPoint()
-ingleCam.showCalib()
+##Show GCPs                           
+#ingleCam.showGCPs()
+#
+##Show Camera plots                          
+#ingleCam.showPrincipalPoint()
+#ingleCam.showCalib()
+
+
+# Optimise camera environment
+ingleCam.optimiseCamEnv('YPR', 'trf', show=False)
 
 #Get inverse projection variables through camera info            
 invprojvars = setProjection(dem, ingleCam._camloc, ingleCam._camDirection, 
                             ingleCam._radCorr, ingleCam._tanCorr, ingleCam._focLen, 
-                            ingleCam._camCen, ingleCam._refImage)
+                            ingleCam._camCen, ingleCam._refImage, viewshed=False)
 
 #Inverse project image coordinates using function from CamEnv object                       
 ingle_xyz = projectUV(ingle_xy[1], invprojvars)
 
 
-# # Optimise camera environment
-# ingleCam.optimiseCamEnv('YPR')
-# opt_projvars = optimiseCamera('YPR', [ingleCam._camloc, ingleCam._camDirection, 
-#                               ingleCam._radCorr, ingleCam._tanCorr, ingleCam._focLen, 
-#                               ingleCam._camCen, ingleCam._refImage], ingle_xy[0], 
-#                               ingle_xy[1], 'trf', show=False)
-
-
-
-#Retrieve DEM from CamEnv object
-demobj=ingleCam.getDEM()
-demextent=demobj.getExtent()
-dem=demobj.getZ()
-
-#Get camera position (xyz) from CamEnv object
-post = ingleCam._camloc
-
-#------------------   Export xyz locations as .txt file   ---------------------
+# #------------------   Export xyz locations as .shp file   ---------------------
 
 print('\n\nSAVING TEXT FILE')
 
 
 #Write xyz coordinates to .txt file
-target1 = destination + 'Ingle_XYZ_projected.txt'
+target1 = destination + '/ingle_xyz.txt'
 f = open(target1, 'w')
 f.write('x' + '\t' + 'y' + '\t' + 'z' + '\n')
 for i in ingle_xyz:
@@ -92,7 +81,7 @@ for i in ingle_xyz:
 f.close()
 
 
-# #------------------   Export xyz locations as .shp file   ---------------------
+#------------------   Export xyz locations as .shp file   ---------------------
 
 print('\n\nSAVING SHAPE FILE')
 
@@ -105,7 +94,7 @@ if driver is None:
 
 
 #Create data source
-shp = destination + 'orthorectify.shp'   
+shp = destination + '/ingle_xyz.shp'   
 if os.path.exists(shp):
     driver.DeleteDataSource(shp)
 ds = driver.CreateDataSource(shp)
@@ -113,21 +102,39 @@ if ds is None:
     print('Could not create file ' + str(shp))
  
        
-# #Set WGS84 projection
-# proj = osr.SpatialReference()
-# proj.ImportFromEPSG(32619)
+#Set WGS84 projection
+proj = osr.SpatialReference()
+proj.ImportFromEPSG(32633)          #CHANGE TO VALID EPSG projection
 
 
-# #Create layer in data source
-# layer = ds.CreateLayer('orthorectify', proj, ogr.wkbPoint)
+#Create layer in data source
+layer = ds.CreateLayer('ingle_xyz', proj, ogr.wkbPoint)
   
   
-# #Add attributes to layer
-# layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))      #ID    
-# layer.CreateField(ogr.FieldDefn('time', ogr.OFTReal))       #Time
-# field_region = ogr.FieldDefn('region', ogr.OFTString)        
-# field_region.SetWidth(8)    
-# layer.CreateField(field_region)                             #Calving region
-# field_style = ogr.FieldDefn('style', ogr.OFTString)        
-# field_style.SetWidth(10)    
-# layer.CreateField(field_style)                              #Calving size    
+#Add attributes to layer
+layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))      #ID    
+    
+  
+#Create point features with data attributes in layer           
+for a in ingle_xyz:
+    count=1
+
+    #Create feature    
+    feature = ogr.Feature(layer.GetLayerDefn())
+
+    #Create feature attributes    
+    feature.SetField('id', count)
+      
+    #Create feature location
+    wkt = "POINT(%f %f)" %  (float(a[0]) , float(a[1]))
+    point = ogr.CreateGeometryFromWkt(wkt)
+    feature.SetGeometry(point)
+    layer.CreateFeature(feature)
+
+    #Free up data space
+    feature.Destroy()                       
+    count=count+1
+
+
+#Free up data space    
+ds.Destroy() 
