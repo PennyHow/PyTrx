524565003b6874774ffc3f1b1b78c2c0a677efa9
sethnavon <79269294+sethnavon@users.noreply.github.com>
2022-05-09 11:22:40 -0400

    Update orthorectification.py

--- Seth_Examples/orthorectification.py
+++ Seth_Examples/orthorectification.py
@@ -15,6 +15,8 @@ import osgeo.ogr as ogr
 import osgeo.osr as osr
 from scipy import interpolate
 from scipy import stats
+from matplotlib.dates import DateFormatter
+import datetime
 
 
 #from pyproj import Proj
@@ -105,21 +107,22 @@ print (slope19, intercept19, r_value19, p_value19)
 
 projectdf_z2019 = mergedf2019[['lineloc', 'stage_filtered']]
 projectdf_z2019['z']= np.nan
-
+projectdf_z2019['underwater'] = np.nan
 for index, row in projectdf_z2019.iterrows():
    # if the lineloc is <= 300, add z value from df['z'] at that index
     if row['lineloc'] <= 300:
         z_row = row['lineloc'].astype(int)
         df_row = df.iloc[z_row]
         row['z'] = df_row['z']
+        row['underwater'] = 1
     # else if lineloc > 300, z-value = slope * lineloc + intercept (y= mx + b)
     elif row['lineloc'] > 300:
         row['z'] = slope19 * row['lineloc'] + intercept19
+        row['underwater'] = 0
+
         
 projectdf_z2019['z_normalized'] = projectdf_z2019['z']-projectdf_z2019['z'].mean()   
 
-projectdf_z2019['bubbler_resampled'] = projectdf_z2019['stage_filtered'].resample('1D').mean()
-projectdf_z2019['z_resampled'] = projectdf_z2019['z_normalized'].resample('1D').mean()
 
 projectdf_z2019['nice_datetimes']= projectdf_z2019.index.strftime("%b %d")
 
@@ -140,6 +143,8 @@ ax1[1].tick_params(axis = 'x', labelrotation = 45)
 
 plt.show()    
 
+projectdf_z2019.to_csv(directory + '/results/projectedresults2019.csv')
+
 ## 2020 ##
 
 mergedf2020 = pd.read_csv(directory + '/results/manual_detection_results2020.csv', parse_dates=['key_0'], index_col='key_0')
@@ -158,21 +163,22 @@ for index1, row1 in mergedf2020_filtered.iterrows():
 
 projectdf_z2020 = mergedf2020[['lineloc', 'stage_filtered']]
 projectdf_z2020['z']= np.nan
-
+projectdf_z2020['underwater'] = np.nan
 for index, row in projectdf_z2020.iterrows():
    # if the lineloc is <= 300, add z value from df['z'] at that index
     if row['lineloc'] <= 300:
         z_row = row['lineloc'].astype(int)
         df_row = df.iloc[z_row]
         row['z'] = df_row['z']
+        row['underwater'] = 1
     # else if lineloc > 300, z-value = slope * lineloc + intercept (y= mx + b)
     elif row['lineloc'] > 300:
         row['z'] = slope19 * row['lineloc'] + intercept19
+        row['underwater'] = 0
+
         
 projectdf_z2020['z_normalized'] = projectdf_z2020['z']-projectdf_z2020['z'].mean()  
 
-projectdf_z2020['bubbler_resampled'] = projectdf_z2020['stage_filtered'].resample('1D').mean()
-projectdf_z2020['z_resampled'] = projectdf_z2020['z_normalized'].resample('1D').mean()  
 
 projectdf_z2020['nice_datetimes']= projectdf_z2020.index.strftime("%b %d")
         
@@ -192,6 +198,7 @@ ax2[0].tick_params(axis = 'x', labelrotation = 45)
 ax2[1].tick_params(axis = 'x', labelrotation = 45)
 
 plt.show()   
+projectdf_z2020.to_csv(directory + '/results/projectedresults2020.csv')
 
 # slope19_corr, intercept19_corr, r_value19_corr, p_value19_corr, std_err19_corr = stats.linregress(projectdf_z2019['stage_filtered'], projectdf_z2019['z'])
 # print (slope19_corr, intercept19_corr, r_value19_corr, p_value19_corr)
@@ -215,23 +222,26 @@ for index1, row1 in mergedf2021_filtered.iterrows():
 projectdf_z2021 = mergedf2021[['lineloc', 'stage_filtered']]
 projectdf_z2021['z']= np.nan
 
+projectdf_z2021['underwater'] = np.nan
 for index, row in projectdf_z2021.iterrows():
    # if the lineloc is <= 300, add z value from df['z'] at that index
     if row['lineloc'] <= 300:
         z_row = row['lineloc'].astype(int)
         df_row = df.iloc[z_row]
         row['z'] = df_row['z']
+        row['underwater'] = 1
     # else if lineloc > 300, z-value = slope * lineloc + intercept (y= mx + b)
     elif row['lineloc'] > 300:
         row['z'] = slope19 * row['lineloc'] + intercept19
+        row['underwater'] = 0
         
 projectdf_z2021['z_normalized'] = projectdf_z2021['z']-projectdf_z2021['z'].mean()    
 
-projectdf_z2021['bubbler_resampled'] = projectdf_z2021['stage_filtered'].resample('1D').mean()
-projectdf_z2021['z_resampled'] = projectdf_z2021['z_normalized'].resample('1D').mean()
 
 
 projectdf_z2021['nice_datetimes']= projectdf_z2021.index.strftime("%b %d")
+
+projectdf_z2021.to_csv(directory + '/results/projectedresults2021.csv')
 
 # fig4, ax4 = plt.subplots(2, constrained_layout = True, sharex = 'col')
 
@@ -253,6 +263,126 @@ projectdf_z2021['nice_datetimes']= projectdf_z2021.index.strftime("%b %d")
 # Scatter plots#
 
 
+
+date_form = DateFormatter("%b %d")
+
+fig6, ax6 = plt.subplots(3, constrained_layout = True)
+# fig6.tight_layout()
+ax6[0].plot(projectdf_z2019.index, projectdf_z2019['z_normalized'], color = 'black', label='Camera Derived Stage')
+ax6[0].plot(projectdf_z2019.index, projectdf_z2019['stage_filtered'], color = 'red', label='Pressure Transducer Stage')
+ax6[0].grid(linestyle='dashed')
+ax6[0].set_ylim(-5, 5)
+ax6[0].set_xlim([datetime.date(2019, 6, 8), datetime.date(2019, 9, 8)])
+ax6[0].xaxis.set_major_formatter(date_form)
+ax6[0].tick_params(axis='both', which='major', direction='out', labelsize=10, width=2)
+
+
+ax6[1].plot(projectdf_z2020.index, projectdf_z2020['z_normalized'], color = 'black')
+ax6[1].plot(projectdf_z2020.index, projectdf_z2020['stage_filtered'], color = 'red')
+ax6[1].grid(linestyle='dashed')
+ax6[1].set_ylim(-5, 5)
+ax6[1].set_xlim([datetime.date(2020, 6, 8), datetime.date(2020, 9, 8)])
+ax6[1].xaxis.set_major_formatter(date_form)
+ax6[1].tick_params(axis='both', which='major', direction='out', labelsize=10, width=2)
+
+
+ax6[2].plot(projectdf_z2021.index, projectdf_z2021['z_normalized'], color = 'black')
+ax6[2].plot(projectdf_z2021.index, projectdf_z2021['stage_filtered'], color = 'red')
+ax6[2].grid(linestyle='dashed')
+ax6[2].set_ylim(-5, 5)
+ax6[2].set_xlim([datetime.date(2021, 6, 8), datetime.date(2021, 9, 8)])
+ax6[2].xaxis.set_major_formatter(date_form)
+ax6[2].tick_params(axis='both', which='major', direction='out', labelsize=10, width=2)
+
+fig6.legend(bbox_to_anchor=(0.72,0))
+# fig6.text(0.04, 0.5, 'Stage (m)', va='center', rotation='vertical')
+
+
+plt.show()
+fig6.savefig(directory + '/results/fig6.png', dpi = 600, bbox_inches='tight')
+
+
+## Linear Regression stats
+#2019
+projectdf_z2019_2 = projectdf_z2019.dropna()
+slope19_hour, intercept19_hour, r_value19_hour, p_value19_hour, std_err19_hour = stats.linregress(projectdf_z2019_2['stage_filtered'],projectdf_z2019_2['z_normalized'])
+print('2019 lin regress values (3 hours): ' +' r squared:' + str(r_value19_hour) + ' std error:' + str(std_err19_hour))
+
+below_water19 = projectdf_z2019_2[projectdf_z2019_2['underwater']==0]
+above_water19 = projectdf_z2019_2[projectdf_z2019_2['underwater']==1]
+
+slope19_hour, intercept19_hour, r_value19_hour, p_value19_hour, std_err19_hour = stats.linregress(below_water19['stage_filtered'],below_water19['z_normalized'])
+print('Extrapolated 2019 lin regress values (3 hours): ' +' r squared:' + str(r_value19_hour) + ' std error:' + str(std_err19_hour))
+slope19_hour, intercept19_hour, r_value19_hour, p_value19_hour, std_err19_hour = stats.linregress(above_water19['stage_filtered'],above_water19['z_normalized'])
+print('orthorectifed 2019 lin regress values (3 hours): ' +' r squared:' + str(r_value19_hour) +' std error:' + str(std_err19_hour))
+
+
+
+projectdf_z2019['bubbler_resampled'] = projectdf_z2019['stage_filtered'].resample('1D').mean()
+projectdf_z2019['z_resampled'] = projectdf_z2019['z_normalized'].resample('1D').mean()
+projectdf_z2019_3 = projectdf_z2019.dropna()
+
+slope19_day, intercept19_day, r_value19_day, p_value19_day, std_err19_day = stats.linregress(projectdf_z2019_3['bubbler_resampled'],projectdf_z2019_3['z_resampled'])
+print('2019 lin regress values (days): ' + ' r squared:' + str(r_value19_day) +' std error:' + str(std_err19_day))
+
+
+
+
+##
+
+
+
+#2020
+projectdf_z2020_2 = projectdf_z2020.dropna()
+slope20_hour, intercept20_hour, r_value20_hour, p_value20_hour, std_err20_hour = stats.linregress(projectdf_z2020_2['stage_filtered'],projectdf_z2020_2['z_normalized'])
+print('2020 lin regress values (3 hours): '  +' r squared:' + str(r_value20_hour) + ' std error:' + str(std_err20_hour))
+
+below_water20 = projectdf_z2020_2[projectdf_z2020_2['underwater']==0]
+above_water20 = projectdf_z2020_2[projectdf_z2020_2['underwater']==1]
+
+slope20_hour, intercept20_hour, r_value20_hour, p_value20_hour, std_err20_hour = stats.linregress(below_water20['stage_filtered'],below_water20['z_normalized'])
+print('Extrapolated 2020 lin regress values (3 hours): ' + 'slope:'  +' r squared:' + str(r_value20_hour)  + ' std error:' + str(std_err20_hour))
+slope20_hour, intercept20_hour, r_value20_hour, p_value20_hour, std_err20_hour = stats.linregress(above_water20['stage_filtered'],above_water20['z_normalized'])
+print('orthorectifed 2020 lin regress values (3 hours): ' + 'slope:'  +' r squared:' + str(r_value20_hour)  + ' std error:' + str(std_err20_hour))
+
+
+projectdf_z2020['bubbler_resampled'] = projectdf_z2020['stage_filtered'].resample('1D').mean()
+projectdf_z2020['z_resampled'] = projectdf_z2020['z_normalized'].resample('1D').mean()  
+projectdf_z2020_3 = projectdf_z2020.dropna()
+
+slope20_day, intercept20_day, r_value20_day, p_value20_day, std_err20_day = stats.linregress(projectdf_z2020_3['bubbler_resampled'],projectdf_z2020_3['z_resampled'])
+print('2020 lin regress values (days): ' + 'slope:'  +' r squared:' + str(r_value20_day) +  ' std error:' + str(std_err20_day))
+
+
+
+#
+
+#2021
+projectdf_z2021_2 = projectdf_z2021.dropna()
+slope21_hour, intercept21_hour, r_value21_hour, p_value21_hour, std_err21_hour = stats.linregress(projectdf_z2021_2['stage_filtered'],projectdf_z2021_2['z_normalized'])
+print('2021 lin regress values (3 hours): ' + ' r squared:' + str(r_value21_hour) + ' std error:' + str(std_err21_hour))
+
+below_water21 = projectdf_z2021_2[projectdf_z2021_2['underwater']==0]
+above_water21 = projectdf_z2021_2[projectdf_z2021_2['underwater']==1]
+
+slope21_hour, intercept21_hour, r_value21_hour, p_value21_hour, std_err21_hour = stats.linregress(below_water21['stage_filtered'],below_water21['z_normalized'])
+print('Extrapolated 2021 lin regress values (3 hours): ' +' r squared:' + str(r_value21_hour) + ' std error:' + str(std_err21_hour))
+slope21_hour, intercept21_hour, r_value21_hour, p_value21_hour, std_err21_hour = stats.linregress(above_water21['stage_filtered'],above_water21['z_normalized'])
+print('orthorectifed 2021 lin regress values (3 hours): '  +' r squared:' + str(r_value21_hour) +  ' std error:' + str(std_err21_hour))
+
+
+
+projectdf_z2021['bubbler_resampled'] = projectdf_z2021['stage_filtered'].resample('1D').mean()
+projectdf_z2021['z_resampled'] = projectdf_z2021['z_normalized'].resample('1D').mean()
+projectdf_z2021_3 = projectdf_z2021.dropna()
+
+slope21_day, intercept21_day, r_value21_day, p_value21_day, std_err21_day = stats.linregress(projectdf_z2021_3['bubbler_resampled'],projectdf_z2021_3['z_resampled'])
+print('2021 lin regress values (days): ' + ' r squared:' + str(r_value21_day) +  ' std error:' + str(std_err21_day))
+
+
+
+#
+
 fig5, ax5 = plt.subplots(nrows=2, ncols=3, sharex = 'col', sharey= 'row')
 
 # fig5.suptitle("Water Level/Pressure Transducer Correlation")
@@ -260,47 +390,53 @@ fig5, ax5 = plt.subplots(nrows=2, ncols=3, sharex = 'col', sharey= 'row')
 ax5[0,0].scatter(projectdf_z2019['stage_filtered'], projectdf_z2019['z_normalized'], facecolors='none', edgecolors='black')
 ax5[0,0].set_title('2019')
 ax5[0,0].grid(linestyle= 'dashed')
+ax5[0,0].set_ylim(bottom = -0.5, top =4.5)
+ax5[0,0].set_xlim(left = -0.5, right =4.5)
 # ax5[0,0].set_xlabel('Filtered Stage (m)')
 # ax5[0,0].set_ylabel('Water level (m)')
 
 ax5[0,1].scatter(projectdf_z2020['stage_filtered'], projectdf_z2020['z_normalized'], facecolors='none', edgecolors='black')
 ax5[0,1].set_title('2020')
 ax5[0,1].grid(linestyle= 'dashed')
-
+ax5[0,1].set_ylim(bottom = -0.5, top =4.5)
+ax5[0,1].set_xlim(left =-0.5, right =4.5)
 # ax5[0,1].set_xlabel('Filtered Stage (m)')
 # ax5[0,1].set_ylabel('Water level (m)')
 
 ax5[0,2].scatter(projectdf_z2021['stage_filtered'], projectdf_z2021['z_normalized'], facecolors='none', edgecolors='black')
 ax5[0,2].set_title('2021')
 ax5[0,2].grid(linestyle= 'dashed')
-
+ax5[0,2].set_ylim(bottom = -0.5, top =4.5)
+ax5[0,2].set_xlim(left = -0.5, right =4.5)
 # ax5[0,2].set_xlabel('Filtered Stage (m)')
 # ax5[0,2].set_ylabel('Water level (m)')
 
 ax5[1,0].scatter(projectdf_z2019['bubbler_resampled'], projectdf_z2019['z_resampled'], facecolors='none', edgecolors='black')
 ax5[1,0].grid(linestyle= 'dashed')
-
+ax5[1,0].set_ylim(bottom = -0.5, top =4.5)
+ax5[1,0].set_xlim(left = -0.5, right =4.5)
 # ax5[1,0].set_title('2019 correlation')
 # ax5[1,0].set_xlabel('Filtered Stage (m)')
 # ax5[1,0].set_ylabel('Water level (m)')
 
 ax5[1,1].scatter(projectdf_z2020['bubbler_resampled'], projectdf_z2020['z_resampled'], facecolors='none', edgecolors='black')
 ax5[1,1].grid(linestyle= 'dashed')
-
+ax5[1,1].set_ylim(bottom =-0.5, top =4.5)
+ax5[1,1].set_xlim(left = -0.5, right =4.5)
 # ax5[1,1].set_title('2020 correlation')
 # ax5[1,1].set_xlabel('Filtered Stage (m)')
 # ax5[0,1].set_ylabel('Water level (m)')
 
 ax5[1,2].scatter(projectdf_z2021['bubbler_resampled'], projectdf_z2021['z_resampled'], facecolors='none', edgecolors='black')
 ax5[1,2].grid(linestyle= 'dashed')
-
+ax5[1,2].set_ylim(bottom = -0.5, top =4.5)
+ax5[1,2].set_xlim(left = -0.5, right =4.5)
 # ax5[1,2].set_title('2021 correlation')
 # ax5[1,2].set_xlabel('Filtered Stage (m)')
 # ax5[0,2].set_ylabel('Water level (m)')
 
-fig5.text(0.5, 0.02, 'Bubbler Stage (m)', ha='center')
-fig5.text(0.04, 0.5, 'Camera Water Level (m)', va='center', rotation='vertical')
-fig5.constrained_layout()
+fig5.text(0.5, 0.02, 'Pressure Transducer Stage (m)', ha='center')
+fig5.text(0.04, 0.5, 'Camera Derived Stage (m)', va='center', rotation='vertical')
 
 # fig5.add_subplot(1, 1, 1, frame_on=False)
 
@@ -311,63 +447,14 @@ fig5.constrained_layout()
 # fig5.xlabel('Filtered Stage (m)', fontsize=15, fontweight='bold')
 # fig5.ylabel('Water Level (m)', fontsize=15, fontweight='bold')
 
-
+fig5.savefig(directory + '/results/fig5.png', dpi = 600)
 plt.show()
 
-fig6, ax6 = plt.subplots(3)
-fig6.tight_layout()
-ax6[0].plot(projectdf_z2019.index, projectdf_z2019['z_normalized'], color = 'black', label='Camera Water Levels')
-ax6[0].plot(projectdf_z2019.index, projectdf_z2019['stage_filtered'], color = 'red', label='Bubbler Stage')
-ax6[0].grid(linestyle='dashed')
-ax6[0].set_ylim(-5, 5)
-ax6[0].set_ylabel('2019')
-ax6[0].set_xticklabels(projectdf_z2019['nice_datetimes'], fontsize=10)
-ax6[0].tick_params(axis='both', which='major', direction='out', labelsize=10, width=2)
 
 
-ax6[1].plot(projectdf_z2020.index, projectdf_z2020['z_normalized'], color = 'black')
-ax6[1].plot(projectdf_z2020.index, projectdf_z2020['stage_filtered'], color = 'red')
-ax6[1].grid(linestyle='dashed')
-ax6[1].set_ylim(-5, 5)
-ax6[1].set_ylabel('2020')
-ax6[1].set_xticklabels(projectdf_z2020['nice_datetimes'], fontsize=10)
-ax6[1].tick_params(axis='both', which='major', direction='out', labelsize=10, width=2)
+
+        
+        
 
 
-ax6[2].plot(projectdf_z2021.index, projectdf_z2021['z_normalized'], color = 'black')
-ax6[2].plot(projectdf_z2021.index, projectdf_z2021['stage_filtered'], color = 'red')
-ax6[2].grid(linestyle='dashed')
-ax6[2].set_ylim(-5, 5)
-ax6[2].set_ylabel('2021')
-ax6[2].set_xticklabels(projectdf_z2021['nice_datetimes'], fontsize=10)
-ax6[2].tick_params(axis='both', which='major', direction='out', labelsize=10, width=2)
 
-# ax6.set_xticks(np.arange(times[0], times[-1], 15))
-# ax6.set_xticklabels(projectdf_z2021['nice_datetimes'], fontsize=10)
-fig6.legend(loc='upper right')
-plt.show()
-
-## Linear Regression stats
-#2019
-projectdf_z2019_2 = projectdf_z2019.dropna()
-slope19_hour, intercept19_hour, r_value19_hour, p_value19_hour, std_err19_hour = stats.linregress(projectdf_z2019_2['stage_filtered'],projectdf_z2019_2['z_normalized'])
-print('2019 lin regress values (3 hours): ' + 'slope:' + str(slope19_hour) + ' intercept:' + str(intercept19_hour) +' r squared:' + str(r_value19_hour) + ' p value:' + str(p_value19_hour) + ' std error:' + str(std_err19_hour))
-# 
-slope19_day, intercept19_day, r_value19_day, p_value19_day, std_err19_day = stats.linregress(projectdf_z2019_2['bubbler_resampled'],projectdf_z2019_2['z_resampled'])
-print('2019 lin regress values (days): ' + 'slope:' + str(slope19_day) + ' intercept:' + str(intercept19_day) +' r squared:' + str(r_value19_day) + ' p value:' + str(p_value19_day) + ' std error:' + str(std_err19_day))
-
-#2020
-projectdf_z2020_2 = projectdf_z2020.dropna()
-slope20_hour, intercept20_hour, r_value20_hour, p_value20_hour, std_err20_hour = stats.linregress(projectdf_z2020_2['stage_filtered'],projectdf_z2020_2['z_normalized'])
-print('2020 lin regress values (3 hours): ' + 'slope:' + str(slope20_hour) + ' intercept:' + str(intercept20_hour) +' r squared:' + str(r_value20_hour) + ' p value:' + str(p_value20_hour) + ' std error:' + str(std_err20_hour))
-# 
-slope20_day, intercept20_day, r_value20_day, p_value20_day, std_err20_day = stats.linregress(projectdf_z2020_2['bubbler_resampled'],projectdf_z2020_2['z_resampled'])
-print('2020 lin regress values (days): ' + 'slope:' + str(slope20_day) + ' intercept:' + str(intercept20_day) +' r squared:' + str(r_value20_day) + ' p value:' + str(p_value20_day) + ' std error:' + str(std_err20_day))
-
-#2021
-projectdf_z2021_2 = projectdf_z2021.dropna()
-slope21_hour, intercept21_hour, r_value21_hour, p_value21_hour, std_err21_hour = stats.linregress(projectdf_z2021_2['stage_filtered'],projectdf_z2021_2['z_normalized'])
-print('2021 lin regress values (3 hours): ' + 'slope:' + str(slope21_hour) + ' intercept:' + str(intercept21_hour) +' r squared:' + str(r_value21_hour) + ' p value:' + str(p_value21_hour) + ' std error:' + str(std_err21_hour))
-# 
-slope21_day, intercept21_day, r_value21_day, p_value21_day, std_err21_day = stats.linregress(projectdf_z2021_2['bubbler_resampled'],projectdf_z2021_2['z_resampled'])
-print('2021 lin regress values (days): ' + 'slope:' + str(slope21_day) + ' intercept:' + str(intercept21_day) +' r squared:' + str(r_value21_day) + ' p value:' + str(p_value21_day) + ' std error:' + str(std_err21_day))
